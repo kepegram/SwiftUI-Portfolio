a9ff6e8cce90cbe2928807ea4d31db12ff1e0776
Kadin Pegram <kepegram1@gmail.com>
2026-07-17 09:40:24 -0500

    Initial Commit

---
+++ SwiftPortfolioApp/SwiftPortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPortfolioAppApp.swift
+//  SwiftPortfolioApp
+//
+//  Created by Kadin Pegram on 7/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
